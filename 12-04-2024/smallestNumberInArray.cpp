7c8478a7d2bcf3a79e5b00fdf08739c8da25d3c7
Shivam kumar <shivamkumar15669@gmail.com>
2024-04-13 17:03:27 +0530

    array

---
+++ 12-04-2024/smallestNumberInArray.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int sortArr(vector<int>&arr){
+     sort(arr.begin(), arr.end());
+     return(arr[0]);
+}
+int main(){
+    vector<int> arr1 = {1,2,3,4,5};
+    vector<int> arr2 = {6,7,8,9,10};
+    cout<<"The smallest number in arr1 is "<< sortArr(arr1)<<endl;
+    cout<<"The smallest number in arr2 is "<< sortArr(arr2)<<endl;
+    return 0 ;
+    
+}
